504457ad05a90dcadb002f6389fbfc41b30fe1b3
H B <bardavidh@yahoo.com>
2022-09-20 23:02:07 -0400

    check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import { fetchWithTimeout } from './services';
+var movies=require('./data/movies.json');
 export function fetchMovies(){
+    const resolveFunction = () => movies;
+    fetchWithTimeout(1000).then(resolveFunction);
     ;
 }
+const moviePromise=fetchMovies();
+
+moviePromise.then(function(results){console.log(results);});

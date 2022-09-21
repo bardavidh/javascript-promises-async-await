d1c175ac46d4e68fffcf2c0451fad2e845b1dfc6
H B <bardavidh@yahoo.com>
2022-09-20 23:08:18 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import { fetchWithTimeout } from './services';
 var movies=require('./data/movies.json');
 export function fetchMovies(){
     const resolveFunction = () => movies;
-    fetchWithTimeout(1000).then(resolveFunction);
+    return fetchWithTimeout(1000).then(resolveFunction);
     ;
 }
 const moviePromise=fetchMovies();
 
-moviePromise.then(function(results){console.log(results);});
+moviePromise.then(results=>{console.log(results);});
